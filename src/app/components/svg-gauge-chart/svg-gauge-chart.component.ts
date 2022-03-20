b10d99982773b377da8338643f8739c43d98a936
Istvan Zsigmond <zistvanz@pttep.com>
2022-03-21 06:34:26 +0700

    svg color and style

--- src/app/components/svg-gauge-chart/svg-gauge-chart.component.ts
+++ src/app/components/svg-gauge-chart/svg-gauge-chart.component.ts
@@ -1,15 +1,16 @@
+import { MAX } from './../data/gauge-chart-data';
 import { Component, Input, OnInit } from '@angular/core';
 import { CURRENT } from '../data/gauge-chart-data';
+import { HtmlParser } from '@angular/compiler';
 @Component({
   selector: 'svg-gauge-chart',
   templateUrl: './svg-gauge-chart.component.html',
   styleUrls: ['./svg-gauge-chart.component.css']
 })
 export class SvgGaugeChartComponent implements OnInit {
-
   @Input() current = CURRENT;
+  @Input() max = MAX;
 
   ngOnInit(): void {
   }
-
 }
